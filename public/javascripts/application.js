131ead6fa2a43c180d996590e0ee7b15ffebeb1d
Jillian <Jillyh0@gmail.com>
2010-07-18 23:08:13 -0400

    adding jumpToSection to the application js for directory pages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+/* used in about and help (obtrusive javascript FTL) */
+function jumpToSection(){
+	var pageIndex = document.form.select.selectedIndex;
+	var newIndex = document.form.select.options[pageIndex].value;
+	if (newIndex)
+		window.location = newIndex;
+}
