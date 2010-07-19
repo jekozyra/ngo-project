965dca662c123570b89686dfad25fca830caed66
Jillian <Jillyh0@gmail.com>
2010-07-18 23:09:00 -0400

    changing toggle behaviour on search results on map and resizing map div

--- public/javascripts/gMaps.js
+++ public/javascripts/gMaps.js
@@ -2,14 +2,34 @@ jQuery.noConflict();
 
 function initialize() {
 	
-	function place_marker(lat, lng, group){
+	function place_marker(lat, lng, group, district, country, district_id, country_id){
+		
+		//searchUrl = "/search/specify?district[name]=" + district_id + "&country[name]=" + country_id + "&display_option=table"
+		//content_link = "<a href = \"#\" onclick=\"jQuery('#results-panel-contents').html('Hello World');\">Dummy link</a>"
+		content_link = "<a href = \"#\" onclick=\"jQuery.ajax({ url: \"/search/search\", context: document.body, success: function(){jQuery('#results-panel-contents').html('Hello World');}});\">Dummy link</a>"
+// 		content: "<a href=\"#\" id=\"more_link\" 
+// onclick=\"try {
+	// $(&quot;details&quot;).visualEffect(&quot;toggle_blind&quot;);
+	// $(&quot;more_link&quot;).update(&quot;Show me less&quot;);} 
+	// catch (e) { alert('RJS error:\n\n' + e.toString()); alert('$(\&quot;details\&quot;).visualEffect(\&quot;toggle_blind\&quot;); \n$(\&quot;more_link\&quot;).update(\&quot;Show me less\&quot;);'); throw e}; return false;\">Show me more</a>"
+		
+		
+	//	var infowindow = new google.maps.InfoWindow({
+        //content: "<a href=\"" + searchUrl + "\" >View results for " + district + ", " + country + "</a>"
+	//		content: content_link
+  //  });
+		
 		var marker = new google.maps.Marker({  
 		  position: new google.maps.LatLng(lat,lng),  
 		  map: map,  
 		  title: group,  
-		  clickable: false,  
+		  //clickable: true,  
 		  icon: "/images/icons/group" + group + ".png" 
 		});
+		
+	//	google.maps.event.addListener(marker, 'click', function() {
+  //    infowindow.open(map,marker);
+  //  });
 	}
 	
   var latlng = new google.maps.LatLng(31.203405,67.807617);
@@ -29,7 +49,7 @@ function initialize() {
 	jQuery.getJSON("/data/map.json",function(json){
 		var markers = json.markers
 		jQuery.each(markers, function(){
-			place_marker(this.lat, this.lng, this.group)
+			place_marker(this.lat, this.lng, this.group, this.district, this.country, this.district_id, this.country_id)
 		});
 
 	});
@@ -63,8 +83,8 @@ jQuery(document).ready(function() {
 		
 		jQuery('#results-panel-btn').click(function(){
 			jQuery('#results-panel-contents').toggle(70);
-			jQuery('#results-panel-h3').toggle(70);
-			jQuery('#results-panel-btn').text(jQuery('#results-panel-btn').text() == 'Show' ? 'Hide' : 'Show');
+			//jQuery('#results-panel-h3').toggle(70);
+			jQuery('#results-panel-btn').text(jQuery('#results-panel-btn').text() == '(Show)' ? '(Hide)' : '(Show)');
 		});
 		
 	});
