d9c9b765f63bf2d9152df6fae12e3e04e1d2ab50
Jillian Kozyra <Jillyh0@gmail.com>
2010-07-20 03:39:03 -0400

    adding the files for the contacts scaffold

---
+++ app/models/contact.rb
@@ -0,0 +1,35 @@
+class Contact < ActiveRecord::Base
+  has_and_belongs_to_many :ngos
+  
+  def show_name
+    self.name.nil? ? "---" : self.name
+  end
+  
+  def show_phone
+    self.phone.nil? ? "---" : self.phone
+  end
+  
+  def show_title
+    self.title.nil? ? "---" : self.title
+  end
+  
+  def show_email
+    self.email.nil? ? "---" : self.email
+  end
+  
+  def show_ngos
+    if self.ngos.nil? or self.ngos.size == 0
+      "---"
+    else
+      names = ""
+      self.ngos.each do |ngo|
+        names += ngo.name
+        unless ngo == self.ngos.last
+          names += ", "
+        end
+      end
+      names
+    end
+  end
+  
+end
