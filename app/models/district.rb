f381f4498554126af942c69f4684e8e0625cc7a2
Jillian Kozyra <Jillyh0@gmail.com>
2010-07-21 00:19:33 -0400

    adding show_province function to disitrict model

--- app/models/district.rb
+++ app/models/district.rb
@@ -2,4 +2,8 @@ class District < ActiveRecord::Base
   belongs_to :country
   belongs_to :province
   has_many :ngos
+  
+  def show_province
+    self.province.nil? ? "---" : self.province.name
+  end
 end
