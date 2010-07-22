a72f0841c0951d3ea8d2d6bd1a7faf6a41eda3d7
Jillian Kozyra <Jillyh0@gmail.com>
2010-07-22 03:32:15 -0400

    altering the chart js so that multiple charts can be displayed on one page with one call

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -10,27 +10,56 @@ class DirectoryController < ApplicationController
   end
   
   def stats
-    @total_ngos = Ngo.count(:id, :distinct => 'true')
-    @total_countries = Ngo.count(:country_id, :distinct => 'true')
-    @total_provinces = Ngo.count(:province_id, :distinct => 'true')
-    @total_districts = Ngo.count(:district_id, :distinct => 'true')
     
+    @charts = {:entries => []}
+    
+    # get the ngos per country
     countries = Ngo.find(:all, :select => "distinct country_id").map{|ngo| ngo.country_id}
-    @countries_ngos = []
-    
+    countries_ngos = []
     countries.each do |country|
       total_ngos = Ngo.count(:id, :distinct => 'true', :conditions => ["country_id = ?", country])
       country_name = Country.find(country).name
-      
-      @countries_ngos << {:chart_label => country_name, :chart_number => total_ngos}
-      
+      countries_ngos << {:chart_label => country_name, :chart_number => total_ngos}
     end
+    country_chart = {:stats => countries_ngos,
+                      :chart_title => "By Country",
+                      :chart_div => "chart_div_country",
+                      :column_label => "Country",
+                      :numeric_label => "Number of Entries"}
+    
+    @charts[:entries] << country_chart
+
+    
+    # get the ngos per sector
+    sectors = Sector.all
+    sectors_ngos = []
+    sectors.each do |sector|
+      total_ngos = Ngo.count_by_sql("SELECT COUNT(distinct ngo_id) FROM ngos_sectors WHERE sector_id = #{sector.id}")
+      sector_name = sector.name
+      sectors_ngos << {:chart_label => sector_name, :chart_number => total_ngos}
+    end
+    sector_chart = {:stats => sectors_ngos,
+                    :chart_title => "By Sector",
+                    :chart_div => "chart_div_sector",
+                    :column_label => "Sector",
+                    :numeric_label => "Number of Entries"}
+    
+    @charts[:entries] << sector_chart
+    
+    
+    
     
     my_file = File.open("#{RAILS_ROOT}/public/data/stats.json", File::WRONLY|File::TRUNC|File::CREAT)
-    my_file.puts "{\"stats\":" + @countries_ngos.to_json.to_s + ",\"chart_title\": \"By Country\"}"
+    my_file.puts @charts.to_json.to_s
     my_file.close
     
     render :layout => "vis_layout"
   end
+
+=begin
+  def test
+    render :layout => "density_layout"
+  end
+=end
 
 end
--- public/javascripts/gStats.js
+++ public/javascripts/gStats.js
@@ -9,27 +9,41 @@ google.setOnLoadCallback(drawChart);
 // Callback that creates and populates a data table, 
 // instantiates the pie chart, passes in the data and
 // draws it.
-
 function drawChart() {
 	
 	// Create our data table.
-	var data = new google.visualization.DataTable();
-
-  data.addColumn('string', 'Country');
-  data.addColumn('number', 'Number of Entries');
-
-	jQuery.getJSON('/data/stats.json', function(chart_data) {
-
-		jQuery.each(chart_data.stats, function(index, value){
-			data.addRows(1);
-		  data.setValue(index, 0, value.chart_label);
-		  data.setValue(index, 1, value.chart_number);
-		});
-	});
+	jQuery.getJSON('/data/stats.json', function(stat_data) {
 		
+		jQuery.each(stat_data.entries, function(num, chart_data){
 
-  // Instantiate and draw our chart, passing in some options.
-	var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-	chart.draw(data, {width: 450, height: 300, title: ""});
+			var data = new google.visualization.DataTable();
+
+			data.addColumn('string', chart_data.column_label);
+		  data.addColumn('number', chart_data.numeric_label);
+
+			jQuery.each(chart_data.stats, function(index, value){
+				data.addRows(1);
+			  data.setValue(index, 0, value.chart_label);
+			  data.setValue(index, 1, value.chart_number);
+			});
+
+
+			// Instantiate and draw our chart, passing in some options.
+			var chart = new google.visualization.PieChart(document.getElementById(chart_data.chart_div));
+			chart.draw(data, {width: 450, height: 300, title: chart_data.chart_title});
+			
+		});
+		
+		
+	});
 	
 }
+
+/*
+stats
+	[chart label
+	 chart number]
+	chart title
+	chart div
+	column label
+	numeric column*/
