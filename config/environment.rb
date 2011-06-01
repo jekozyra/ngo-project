48162325e85e830a3e8477d9049c309fcdfdbddb
Jillian <Jillyh0@gmail.com>
2011-06-01 18:50:11 +0200

    adding line to environment file hopefully to eliminate error

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,10 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
+require 'thread'
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
@@ -20,13 +21,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
-  config.gem(
-    'thinking-sphinx',
-    :lib     => 'thinking_sphinx',
-    :version => '1.3.18'  )
+  config.gem('thinking-sphinx', :lib => 'thinking_sphinx', :version => '1.3.18'  )
 
-
-  config.gem('riddle', :version => '1.0.12')  
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
